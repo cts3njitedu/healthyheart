cdc636122062eccbbff61824381e39ff7f5ab999
Craig Steele <csteele23@Craigs-MacBook-Pro.local>
2019-12-27 15:07:30 -0500

    change how i access port

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/cts3njitedu/healthyheart
 
 go 1.13
 
-require github.com/dustin/go-humanize v1.0.0
+require (
+	github.com/dustin/go-humanize v1.0.0
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
+	golang.org/x/tools v0.0.0-20191227053925-7b8e75db28f4 // indirect
+)
--- main.go
+++ main.go
@@ -19,5 +19,14 @@ func main() {
 	http.HandleFunc("/about", about)
 	fmt.Println("Server Starting...")
 	fmt.Println(humanize.Bytes(82854982))
-	http.ListenAndServe(":3000", nil)
+	
+	var port = os.Getenv("PORT")
+		// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "3000"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+
+	http.ListenAndServe(port, nil)
 }
